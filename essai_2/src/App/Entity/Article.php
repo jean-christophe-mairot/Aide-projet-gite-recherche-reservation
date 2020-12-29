c6c29d14f8e74e00b6d976346d15806ea2c4420b
jc mairot <j.mairot@codeur.online>
2020-12-29 10:12:43 +0100

    prob variable non declarées fin 10

--- essai_2/src/App/Entity/Article.php
+++ essai_2/src/App/Entity/Article.php
@@ -4,8 +4,28 @@ namespace App\Entity;
 class Article{
 
 
+    
+    /**
+     * name
+     *
+     * @var string  $name => nom de l'article
+     */
+    private $name;
 
-    private $name
 
+    
 
+    /**
+     * Set $name => nom de l'article
+     *
+     * @param  string  $name  $name => nom de l'article
+     *
+     * @return  self
+     */ 
+    public function setName(string $name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
--- essai_2/web/index.php
+++ essai_2/web/index.php
@@ -1,3 +1,19 @@
 <?php
-require_once('../src/App/Entity/Article2.php'); 
+require_once('../src/App/Entity/Article.php'); 
 use App\Entity\Article;
+
+$article= new Article();
+
+$article->setName('voiture');
+
+echo '<pre>';
+var_dump($article);
+echo '</pre>';
+
+//comme aucn attribut de déclaré pour $price dans la class Article
+//l'attribut est considérer commer public par defa
+
+$article->price=10;
+echo '<pre>';
+var_dump($article);
+echo '</pre>';
