d8ed37d854969cc2cde6511a229d1e3c89ee7b82
jctest <jcmairot@gmail.com>
2021-01-04 11:21:53 +0100

    poo entité

---
+++ crud_ poo/src/App/Entity/Admin.php
@@ -0,0 +1,12 @@
+<?php
+//https://www.youtube.com/watch?v=Dgsy76UNoGc&list=PLXGXMIp685itWVxDsZzUacR-5Jtt0QSIM&ab_channel=EmmanuelRavrat
+namespace App\Entity;
+
+class Admin{
+
+    private int $id_admin;
+    private string $email;
+    private string $password;
+
+
+}
