b8d06e301385c8e29d3998e5a7901954aac66798
jc mairot <j.mairot@codeur.online>
2021-01-19 10:49:17 +0100

    visibilité des attribut methodes

--- poo_108012021/class/Compte.php
+++ poo_108012021/class/Compte.php
@@ -1,4 +1,8 @@
 <?php
+// quand les attributs sont en private on fait des getter setter
+// self c la classe 
+// $this c l instance de la classe
+// :void -> retoune rien, vide
 
 /**
  * class Compte bancaire
@@ -10,15 +14,18 @@ class Compte
      *
      * @var string $titulaire user du compte
      */
-    public $titulaire;    
+    private string $titulaire;    
 
     /**
      * solde
      *
      * @var float $solde solde du compte 
      */
-    public $solde;
-        
+    private float $solde;
+    
+    //constante pour y accéder on passe par la classe
+    const TAUX_INTERETS = 5/100 ;
+
     /**
      * Method __construct
      * si le solde n'est pas renseigner on lui donne 100 euro (float $solde =100)
@@ -33,20 +40,77 @@ class Compte
     {
         // on attribut le nom à la propriété titulaire de l'instance créée 
         $this->titulaire = $titulaire;
-        $this->solde = $solde;
+        // self le nom de la classe
+        $this->solde = $solde + ($solde * self::TAUX_INTERETS);
+        //pour afficher si le titulaire est à decouvert lors de la création de l'instance
+        echo $this->decouvert();
+
     }
     
+    /**
+     * Get $solde solde du compte
+     * retoune le solde du compte
+     * @return  float
+     */ 
+    public function getSolde(): float
+    {
+        return $this->solde;
+    }
+
+    /**
+     * Set $solde solde du compte
+     * modifie le solde du compte
+     * @param  float  $solde  $solde solde du compte
+     *
+     * @return  self
+     */ 
+    public function setSolde(float $solde): self
+    {
+        if($solde>=0){
+            $this->solde = $solde;    
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get $titulaire user du compte
+     *
+     * @return  string
+     */ 
+    public function getTitulaire(): string
+    {
+        return $this->titulaire;
+    }
+
+    /**
+     * Set $titulaire user du compte
+     * pour changer le nom d'un titulaire
+     * @param  string  $titulaire  $titulaire user du compte
+     *
+     * @return  self
+     */ 
+    public function setTitulaire(string $titulaire): self
+    {
+        if($titulaire != ""){
+            $this->titulaire = $titulaire;
+        }
+        return $this;
+    }
+
+    // ***************************** METHODE **************************************
+
     /**
      * Method deposer
      *
      * @param float $montant [explicite description]
      *
      */
-    public function deposer(float $montant)
+    public function deposer(float $solde)
     {
-        //verifi si le montant est possitif
-        if($montant>0){
-            $this->montant += $montant;
+        //verifi si le montant est positif
+        if($solde>0){
+            $this->solde += $solde;
         }
     }
 
@@ -76,5 +140,34 @@ class Compte
         }else{
             echo "Montant invalide ou solde insuffisant";
         }
+        // 
+        echo $this->decouvert();
     }
+    
+    /**
+     * decouvert check si le titualire est à decouvert
+     * 
+     * @return void
+     */
+    private function decouvert()
+    {
+        if($this->solde<0){
+            return "Vous êtes à decouvert";
+        }else{
+            return "vous n'êtes pas à decouvert";
+        }
+    }
+
+    
+    /**
+     * __toString :
+     * methode magique pour la conversion en chaine de caractères
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return "vous visualisez le compte de $this->titulaire, le solde est de $this->solde euros";
+    }
+
 }
--- poo_108012021/index.php
+++ poo_108012021/index.php
@@ -8,9 +8,12 @@ $compte1 = new Compte('jc',500);
 // $compte1->titulaire='jc';
 // $compte1->solde=500;
 
+
+$compte1->setSolde(1500);
 // on depose 500 euro
 $compte1->deposer(500);
 $compte1->retirer(165);
+
 
 $compte2 = new Compte('robert',389.25);
 // $compte2->titulaire='robert';
@@ -22,8 +25,12 @@ $compte2->voirSolde();
 var_dump($compte1);
 echo'<br>';
 var_dump($compte2);
+
+//  accé à la constante avec nom de la classe :: nom de la constante
+echo "le taux d'intérêt du compte est : " . Compte::TAUX_INTERETS . "%";
 ?>
 
 <p><?php $compte2->voirsolde()?></p>
 
+<p><?php $compte1->voirsolde()?></p>
 
